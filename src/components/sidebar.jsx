7ecd8e5042494959f1799f5a91de9b801c162a0f
GhostofUchiha18 <shivanapit8@gmail.com>
2024-09-10 12:04:28 +0545

    hover done

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = ({ children }) => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const menuItems = [
-    { name: "Attendance", path: "/attendance", icon: <NotepadText /> },
+    { name:"Attendance", path: "/attendance", icon: <NotepadText /> },
     { name: "Users", path: "/users", icon: <UserCog /> },
   ];
 
@@ -18,7 +18,7 @@ const Sidebar = ({ children }) => {
 
   return (
     <div className="flex">
-      <div className="flex flex-col fixed top-0 left-0 h-screen w-64 bg-blue-900 text-white p-5 font-medium">
+      <div className="flex flex-col fixed top-0 left-0 h-screen w-64 bg-blue-900 text-white font-medium border-white">
         <img src="/LogoRed.png" alt="Logo" />
         <nav>
           <ul className="space-y-4">
@@ -26,8 +26,8 @@ const Sidebar = ({ children }) => {
               <li key={index}>
                 <button
                   onClick={() => handleNavigation(item.path, index)}
-                  className={`flex items-center text-left p-2 rounded-md transition-colors duration-200 ${
-                    activeIndex === index ? "bg-white text-blue-900" : "hover:bg-white text-white hover:text-blue-900"
+                  className={`flex items-center text-left p-2 transition-colors duration-200 ${
+                    activeIndex === index ? "bg-white text-blue-900 w-full" : ""
                   }`}
                 >
                   {item.icon}
